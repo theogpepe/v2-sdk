d195c028203338afdd9d57e9dc87b2c5ec5fbce2
Lint Action <lint-action@samuelmeuli.com>
2024-01-05 22:42:56 +0000

    Fix code style issues with Prettier

--- dist/constants.d.ts
+++ dist/constants.d.ts
@@ -1,34 +1,34 @@
-import JSBI from 'jsbi';
-export declare type BigintIsh = JSBI | bigint | string;
+import JSBI from 'jsbi'
+export declare type BigintIsh = JSBI | bigint | string
 export declare enum ChainId {
-    MAINNET = 1
+  MAINNET = 1
 }
 export declare enum TradeType {
-    EXACT_INPUT = 0,
-    EXACT_OUTPUT = 1
+  EXACT_INPUT = 0,
+  EXACT_OUTPUT = 1
 }
 export declare enum Rounding {
-    ROUND_DOWN = 0,
-    ROUND_HALF_UP = 1,
-    ROUND_UP = 2
+  ROUND_DOWN = 0,
+  ROUND_HALF_UP = 1,
+  ROUND_UP = 2
 }
-export declare const FACTORY_ADDRESS = "0x52fbA58f936833F8b643e881Ad308b2e37713a86";
-export declare const INIT_CODE_HASH = "0xe52bc03495182c6d1b6a15111e963ba37f23e9a01347aa78328f88f086b80504";
-export declare const MINIMUM_LIQUIDITY: JSBI;
-export declare const ZERO: JSBI;
-export declare const ONE: JSBI;
-export declare const TWO: JSBI;
-export declare const THREE: JSBI;
-export declare const FIVE: JSBI;
-export declare const TEN: JSBI;
-export declare const _100: JSBI;
-export declare const _997: JSBI;
-export declare const _1000: JSBI;
+export declare const FACTORY_ADDRESS = '0x52fbA58f936833F8b643e881Ad308b2e37713a86'
+export declare const INIT_CODE_HASH = '0xe52bc03495182c6d1b6a15111e963ba37f23e9a01347aa78328f88f086b80504'
+export declare const MINIMUM_LIQUIDITY: JSBI
+export declare const ZERO: JSBI
+export declare const ONE: JSBI
+export declare const TWO: JSBI
+export declare const THREE: JSBI
+export declare const FIVE: JSBI
+export declare const TEN: JSBI
+export declare const _100: JSBI
+export declare const _997: JSBI
+export declare const _1000: JSBI
 export declare enum SolidityType {
-    uint8 = "uint8",
-    uint256 = "uint256"
+  uint8 = 'uint8',
+  uint256 = 'uint256'
 }
 export declare const SOLIDITY_TYPE_MAXIMA: {
-    uint8: JSBI;
-    uint256: JSBI;
-};
+  uint8: JSBI
+  uint256: JSBI
+}
--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@ import JSBI from 'jsbi'
 export type BigintIsh = JSBI | bigint | string
 
 export enum ChainId {
-  MAINNET = 1,
+  MAINNET = 1
 }
 
 export enum TradeType {
