91c4a3ce71a6df1fa7bf22b659da6740cdde8bc9
wojakpepechad <tokenalert7@gmail.com>
2024-01-05 22:44:44 +0100

    theogpepe

---
+++ src/constants.ts
@@ -0,0 +1,46 @@
+import JSBI from 'jsbi'
+
+// exports for external consumption
+export type BigintIsh = JSBI | bigint | string
+
+export enum ChainId {
+  MAINNET = 1,
+}
+
+export enum TradeType {
+  EXACT_INPUT,
+  EXACT_OUTPUT
+}
+
+export enum Rounding {
+  ROUND_DOWN,
+  ROUND_HALF_UP,
+  ROUND_UP
+}
+
+export const FACTORY_ADDRESS = '0x52fbA58f936833F8b643e881Ad308b2e37713a86'
+
+export const INIT_CODE_HASH = '0xe52bc03495182c6d1b6a15111e963ba37f23e9a01347aa78328f88f086b80504'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const TWO = JSBI.BigInt(2)
+export const THREE = JSBI.BigInt(3)
+export const FIVE = JSBI.BigInt(5)
+export const TEN = JSBI.BigInt(10)
+export const _100 = JSBI.BigInt(100)
+export const _997 = JSBI.BigInt(990)
+export const _1000 = JSBI.BigInt(1000)
+
+export enum SolidityType {
+  uint8 = 'uint8',
+  uint256 = 'uint256'
+}
+
+export const SOLIDITY_TYPE_MAXIMA = {
+  [SolidityType.uint8]: JSBI.BigInt('0xff'),
+  [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+}
---
+++ test/constants.test.ts
@@ -0,0 +1,20 @@
+import { INIT_CODE_HASH } from '../src/constants'
+
+//import { bytecode } from '@uniswap/v2-core/build/UniswapV2Pair.json'
+//import { keccak256 } from '@ethersproject/solidity'
+
+// this _could_ go in constants, except that it would cost every consumer of the sdk the CPU to compute the hash
+// and load the JSON.
+//const COMPUTED_INIT_CODE_HASH = keccak256(['bytes'], [`0x${bytecode}`])
+
+describe('constants', () => {
+  describe('INIT_CODE_HASH', () => {
+    // it('matches computed bytecode hash', () => {
+    //   expect(COMPUTED_INIT_CODE_HASH).toEqual(INIT_CODE_HASH)
+    // })
+    //TODO: Replace with real IgniPair.json
+    it('matches computed bytecode hash', () => {
+      expect(INIT_CODE_HASH).toEqual('0x883fb58464f65c11b31140b18ada41e7a0edae8695ffedbd43d3b6150e8375cb')
+    })
+  })
+})
